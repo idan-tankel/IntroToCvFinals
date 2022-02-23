2f4815619a042da09dc379256b62d0d88bb48aef
idan-tankel <shakel002@gmail.com>
2022-02-23 23:59:15 +0200

    start writing some geo imports

---
+++ graphnn.py
@@ -0,0 +1,35 @@
+import os
+
+# For downloading pre-trained models
+import urllib.request
+from urllib.error import HTTPError
+
+# PyTorch Lightning
+import pytorch_lightning as pl
+
+# PyTorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+# PyTorch geometric
+import torch_geometric
+import torch_geometric.data as geom_data
+import torch_geometric.nn as geom_nn
+
+# PL callbacks
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+AVAIL_GPUS = min(1, torch.cuda.device_count())
+BATCH_SIZE = 64
+DATASET_PATH = os.environ.get("PATH_DATASETS", "data/")
+# Path to the folder where the pretrained models are saved
+CHECKPOINT_PATH = os.environ.get("PATH_CHECKPOINT", "saved_models/GNNs/")
+
+# Setting the seed
+pl.seed_everything(42)
+
+# Ensure that all operations are deterministic on GPU (if used) for reproducibility
+torch.backends.cudnn.determinstic = True
+torch.backends.cudnn.benchmark = False
